c002d2b9fce53767f401c9ffd5471caaee2f40a7
joseph walter <josephwalter@josephs-MacBook-Pro.local>
2021-01-18 20:00:06 -0700

    installed bootstrap and created a main page

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>This is the Main Page</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Main',
+    props:{
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
